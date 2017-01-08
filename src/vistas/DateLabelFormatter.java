098e14bf8e224e777bacc6c8557f186642b33b9f
Paula <paulaurteaga@opendeusto.es>
2017-01-08 12:11:02 +0100

    Tema de fechas con libreria externa

---
+++ src/vistas/DateLabelFormatter.java
@@ -0,0 +1,27 @@
+package vistas;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.swing.JFormattedTextField.AbstractFormatter;
+
+import Gestor.GestorProductos;
+
+@SuppressWarnings("serial")
+public class DateLabelFormatter extends AbstractFormatter {
+    private String datePattern = GestorProductos.formatoFechas;
+    private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+    @Override
+    public Object stringToValue(String text) throws ParseException {
+        return dateFormatter.parseObject(text);
+    }
+    @Override
+    public String valueToString(Object value) throws ParseException {
+        if (value != null) {
+            Calendar cal = (Calendar) value;
+            return dateFormatter.format(cal.getTime());
+        }
+        return "";
+    }
+};
--- src/vistas/VentanaInformeFecha.java
+++ src/vistas/VentanaInformeFecha.java
@@ -12,6 +12,10 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.table.DefaultTableModel;
 
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
 import Gestor.GestorProductos;
 
 import java.awt.event.ActionListener;
@@ -19,15 +23,17 @@ import java.awt.event.ActionEvent;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Properties;
 import java.util.Vector;
+
+import javax.swing.SpringLayout;
 
 public class VentanaInformeFecha extends JFrame{
 
 	private JTable table;
 	private JScrollPane scroll;
-	private JTextField textField;
-	private JTextField textField_1;
 	private ArrayList<String[]> registro;
 	int posicionNombre = 0;
 	int posicionApellido = 1;
@@ -36,6 +42,8 @@ public class VentanaInformeFecha extends JFrame{
 	int posicionFecha=4;
 	int posicionProducto=5;
 	int posicionPrecio=6;
+	private JDatePickerImpl datePicker;
+	private JDatePickerImpl datePicker_1;
 	public VentanaInformeFecha()
 	{
 		registro= GestorProductos.leerRegistro();
@@ -49,33 +57,35 @@ public class VentanaInformeFecha extends JFrame{
 		lblNewLabel.setBounds(127, 37, 56, 14);
 		getContentPane().add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(205, 34, 86, 20);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+		 UtilDateModel model = new UtilDateModel();
+	        Properties prop = new Properties();
+	        prop.put("text.today", "Hoy");
+	        prop.put("text.month", "Mes");
+	        prop.put("text.year", "Año");
+	        JDatePanelImpl datePanel = new JDatePanelImpl(model, prop);
+	        datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+	        SpringLayout springLayout = (SpringLayout) datePicker.getLayout();
+	        springLayout.putConstraint(SpringLayout.SOUTH, datePicker.getJFormattedTextField(), 0, SpringLayout.SOUTH, datePicker);
+	        datePicker.setBounds(205,34,119,23);
+	        getContentPane().add(datePicker);
 		
 		JLabel lblNewLabel_1 = new JLabel("Hasta");
 		lblNewLabel_1.setBounds(375, 37, 46, 14);
 		getContentPane().add(lblNewLabel_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(488, 34, 86, 20);
-		getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		 UtilDateModel model_1 = new UtilDateModel();
+	        JDatePanelImpl datePanel_1 = new JDatePanelImpl(model_1, prop);
+	        datePicker_1 = new JDatePickerImpl(datePanel_1, new DateLabelFormatter());
+	        datePicker_1.setBounds(488,34,119,20);
+	        getContentPane().add(datePicker_1);
 		
 		JButton btnBuscar = new JButton("Buscar");
 		btnBuscar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				String desde= textField.getText();
-				String hasta= textField_1.getText();
-				SimpleDateFormat formatter = new SimpleDateFormat(GestorProductos.formatoFechas);
+				Date fechainicio = (Date) datePicker.getModel().getValue();
+				Date fechafin = (Date) datePicker_1.getModel().getValue();
+				actualizarTabla(fechainicio,fechafin);
 				
-				try {
-					Date inicio = formatter.parse(desde);
-					Date fin = formatter.parse(hasta);
-					actualizarTabla(inicio,fin);
-				} catch (ParseException e) {
-				}
 			}
 		});
 		btnBuscar.setBounds(661, 33, 89, 23);
@@ -91,6 +101,13 @@ public class VentanaInformeFecha extends JFrame{
 		Vector<Vector<String>> datostabla= new Vector<Vector<String>>();
 		Vector<String> lineatabla;
 		
+		inicio.setSeconds(0);
+		inicio.setMinutes(0);
+		inicio.setHours(0);
+		fin.setSeconds(59);
+		fin.setMinutes(59);
+		fin.setHours(23);
+		
 		for (int j=0;j<registro.size();j++)
 		{
 			linea = registro.get(j);
@@ -103,6 +120,10 @@ public class VentanaInformeFecha extends JFrame{
 				JOptionPane.showMessageDialog(null, "La fecha encontrada en el registro no se ha podido convertir en objeto de tipo fecha");
 				continue;
 			}
+			
+			fecha.setSeconds(0);
+			fecha.setMinutes(0);
+			fecha.setHours(12);
 			
 			if (fecha.compareTo(inicio)>=0 && fecha.compareTo(fin)<=0)
 			{
